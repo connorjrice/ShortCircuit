016e39c9addb0f7d04a08ffd043f201e987240cc
Connor <Connor@clarencedesktop>
2014-04-13 17:36:02 -0600

    Didn't do much

---
+++ src/ShortCircuit/TowerProfileMain.java
@@ -0,0 +1,45 @@
+package ShortCircuit;
+
+import ShortCircuit.GUI.StartGUI;
+import ShortCircuit.Tower.MainState.TowerMainState;
+import com.jme3.app.SimpleApplication;
+import com.jme3.system.AppSettings;
+
+/**
+ *
+ * @author Connor
+ */
+public class TowerProfileMain extends SimpleApplication {
+    private TowerMainState tms;
+    
+    
+    public static void main(String[] args) {
+        TowerProfileMain app = new TowerProfileMain();
+        AppSettings sets = new AppSettings(true);
+        sets.setResolution(1920,1080);
+        sets.setFrequency(60);
+        sets.setFullscreen(false);
+        sets.setVSync(true);
+        sets.setFrameRate(60);
+        app.setSettings(sets);
+        app.setShowSettings(false);
+        app.start();
+    }
+    private StartGUI sgui;
+
+    @Override
+    public void simpleInitApp() {
+        flyCam.setDragToRotate(true);
+        flyCam.setRotationSpeed(0.0f);
+        flyCam.setZoomSpeed(0.0f);
+        setDisplayFps(false);
+        setDisplayStatView(false);
+        startTower();
+    }
+        
+    public void startTower() {
+
+    }
+    
+    
+}
---
+++ src/ShortCircuit/Transit/GUI/TransitGUI.java
@@ -0,0 +1,32 @@
+package ShortCircuit.Transit.GUI;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+
+/**
+ *
+ * @author Connor
+ */
+public class TransitGUI extends AbstractAppState {
+    
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        //TODO: initialize your AppState, e.g. attach spatials to rootNode
+        //this is called on the OpenGL thread after the AppState has been attached
+    }
+    
+    @Override
+    public void update(float tpf) {
+        //TODO: implement behavior during runtime
+    }
+    
+    @Override
+    public void cleanup() {
+        super.cleanup();
+        //TODO: clean up what you initialized in the initialize method,
+        //e.g. remove all spatials from rootNode
+        //this is called on the OpenGL thread after the AppState has been detached
+    }
+}
