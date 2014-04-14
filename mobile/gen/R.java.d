0e49ca25bba04dc5fe6f174893f5c28d6dd7f47c
= <connor.rice@coloradocollege.edu>
2014-04-14 10:53:46 -0600

    Problems with netbeans
    Hopefully, this will solve them

--- mobile/gen/R.java.d
+++ mobile/gen/R.java.d
@@ -1,20 +1,16 @@
 C:\Users\Connor\Documents\ShortCircuit\mobile\gen\shortcircuit\game\R.java \
  : C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-hdpi\ic_launcher.png \
-C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-hdpi\loading.png \
+C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-hdpi\loading.jpg \
 C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-ldpi\ic_launcher.png \
-C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-ldpi\loading.png \
+C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-ldpi\loading.jpg \
 C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-mdpi\ic_launcher.png \
-C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-mdpi\loading.png \
+C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-mdpi\loading.jpg \
 C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-xhdpi\ic_launcher.png \
-C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-xhdpi\loading.png \
+C:\Users\Connor\Documents\ShortCircuit\mobile\res\drawable-xhdpi\loading.jpg \
 C:\Users\Connor\Documents\ShortCircuit\mobile\res\layout\main.xml \
 C:\Users\Connor\Documents\ShortCircuit\mobile\res\values\strings.xml \
 C:\Users\Connor\Documents\ShortCircuit\mobile\bin\res\drawable-hdpi\ic_launcher.png \
-C:\Users\Connor\Documents\ShortCircuit\mobile\bin\res\drawable-hdpi\loading.png \
 C:\Users\Connor\Documents\ShortCircuit\mobile\bin\res\drawable-ldpi\ic_launcher.png \
-C:\Users\Connor\Documents\ShortCircuit\mobile\bin\res\drawable-ldpi\loading.png \
 C:\Users\Connor\Documents\ShortCircuit\mobile\bin\res\drawable-mdpi\ic_launcher.png \
-C:\Users\Connor\Documents\ShortCircuit\mobile\bin\res\drawable-mdpi\loading.png \
 C:\Users\Connor\Documents\ShortCircuit\mobile\bin\res\drawable-xhdpi\ic_launcher.png \
-C:\Users\Connor\Documents\ShortCircuit\mobile\bin\res\drawable-xhdpi\loading.png \
 C:\Users\Connor\Documents\ShortCircuit\mobile\bin\AndroidManifest.xml \
